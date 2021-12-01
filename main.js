915962629ef264a628b07794b5631d0006dc26f5
gelis07 <68599597+gelis07@users.noreply.github.com>
2021-12-01 20:37:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,19 @@ var start = new Date(now.getFullYear(), 0, 0);
 var diff = now - start;
 var oneDay = 1000 * 60 * 60 * 24;
 var day = Math.floor(diff / oneDay)
+var BussinessDays = 0;
 var daysLeft = 359 - day;
+var currentDate = new Date();
+for (let i = 0; i < daysLeft; i++) {
+    currentDate.setDate(now.getDate()+i);
+    var currentDateName = days[currentDate.getDay()]
+    console.log(currentDateName)
+    if(currentDateName == "Monday" || currentDateName == "Tuesday" || currentDateName == "Wednesday" || currentDateName == "Thursday" || currentDateName == "Friday"){
+        BussinessDays += 1;
+        console.log(i);
+    }
+} 
 document.getElementById("date").innerHTML = "Today: " + date;
 document.getElementById("daysLeft").innerHTML = "Days left: " + daysLeft;
+document.getElementById("BdaysLeft").innerHTML = "Bussiness Days left: " + (BussinessDays - 1);
 document.getElementById("ChristmasDate").innerHTML = "Christmas: " + Christmas.getDate() +"/"+(Christmas.getMonth()+1)+"/"+Christmas.getFullYear()+"  " + ChristmasDay;
